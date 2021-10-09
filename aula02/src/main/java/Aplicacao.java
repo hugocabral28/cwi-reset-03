f1428ad0f540dc7f096d4aa17e82a070ccd09aa1
hugo.cabral <hugocabral2803@gmail.com>
2021-10-09 12:19:00 -0300

    Criado class Ator e ENUM genero

--- aula02/src/main/java/Aplicacao.java
+++ aula02/src/main/java/Aplicacao.java
@@ -1,6 +1,7 @@
 public class Aplicacao {
     public static void main(String[] args){
-        Diretor diretor1 =new Diretor("Diretor1",39,2);
+        Diretor diretor1 =new Diretor("Diretor1",39,2,Genero.FEMININO);
+        Ator ator = new Ator("Ator 1",32,3,Genero.MASCULINO);
 
         Filme filme1 = new Filme("Nome do filme","Terro",190,2019,5,diretor1);
         Filme filme2 = new Filme("Nome do filme2","Aventura",182,2020,4,diretor1);
@@ -8,5 +9,9 @@ public class Aplicacao {
         filme1.reproduzirFilme();
         System.out.println("______________");
         filme2.reproduzirFilme();
+        System.out.println("______________");
+        diretor1.imprimarDados();
+        ator.imprimarDados();
+
     }
 }
--- aula02/src/main/java/Ator.java
+++ aula02/src/main/java/Ator.java
@@ -1,15 +1,16 @@
-public class Diretor {
+public class Ator {
     private String nome;
     private int idade;
-    private int quantidadeFilmes;
+    private int oscarsVencidos;
     private Genero genero;
 
-    public Diretor(String nome, int idade, int quantidadeFilmes , Genero genero) {
+    public Ator(String nome, int idade, int oscarsVencidos, Genero genero) {
         this.nome = nome;
         this.idade = idade;
-        this.quantidadeFilmes = quantidadeFilmes;
+        this.oscarsVencidos = oscarsVencidos;
         this.genero = genero;
     }
+
     public void imprimarDados(){
         System.out.println("Nome: "+ getNome());
         System.out.println("Idade: "+ getIdade());
@@ -32,11 +33,19 @@ public class Diretor {
         this.idade = idade;
     }
 
-    public int getQuantidadeFilmes() {
-        return quantidadeFilmes;
+    public int getOscarsVencidos() {
+        return oscarsVencidos;
     }
 
-    public void setQuantidadeFilmes(int quantidadeFilmes) {
-        this.quantidadeFilmes = quantidadeFilmes;
+    public void setOscarsVencidos(int oscarsVencidos) {
+        this.oscarsVencidos = oscarsVencidos;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
     }
 }
