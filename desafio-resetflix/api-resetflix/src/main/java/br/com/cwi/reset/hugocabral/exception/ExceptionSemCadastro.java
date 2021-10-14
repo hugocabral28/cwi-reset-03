5a8f91617d708ccafa33802adb1e6ecd1910239e
hugo.cabral <hugocabral2803@gmail.com>
2021-10-14 02:25:17 -0300

    Modified ExceptionSemCadastro

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/exception/ExceptionSemCadastro.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/exception/ExceptionSemCadastro.java
@@ -2,6 +2,6 @@ package br.com.cwi.reset.hugocabral.exception;
 
 public class ExceptionSemCadastro extends Exception{
     public ExceptionSemCadastro(String nome) {
-        System.out.println("Nenhum " + nome + " cadastrado, favor cadastar "+ nome + ".");
+        System.out.println("Nenhum " + nome + " cadastrado, favor cadastar atores.");
     }
 }
