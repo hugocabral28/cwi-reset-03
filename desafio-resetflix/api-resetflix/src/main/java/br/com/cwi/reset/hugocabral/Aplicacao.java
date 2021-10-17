f7c5c8732cead5f3e6400f787897c4abb1f25ead
hugo.cabral <hugocabral2803@gmail.com>
2021-10-17 19:32:49 -0300

    Transformando em aplicação Web

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/Aplicacao.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/Aplicacao.java
@@ -1,45 +1,13 @@
 package br.com.cwi.reset.hugocabral;
 
-import br.com.cwi.reset.hugocabral.domain.Ator;
-import br.com.cwi.reset.hugocabral.domain.StatusCarreira;
-import br.com.cwi.reset.hugocabral.request.AtorRequest;
-import br.com.cwi.reset.hugocabral.service.AtorService;
-import br.com.cwi.reset.hugocabral.teste.ator.TestAtor;
-import br.com.cwi.reset.hugocabral.teste.diretor.TestDiretor;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Aplicacao {
 
     public static void main(String[] args) throws Exception {
-        FakeDatabase fakeDatabase = new FakeDatabase();
-        AtorService atorService = new AtorService(fakeDatabase);
-
-        String nome = "Will Smith";
-        LocalDate dataNascimento = LocalDate.of(1968, Month.SEPTEMBER, 25);
-        StatusCarreira statusCarreira = StatusCarreira.EM_ATIVIDADE;
-        Integer anoInicioAtividade = 1986;
-
-        AtorRequest atorRequest = new AtorRequest(nome, dataNascimento, statusCarreira, anoInicioAtividade);
-        atorService.criarAtor(atorRequest);
-
-        List<Ator> atores = fakeDatabase.recuperaAtores();
-
-        System.out.println("Deve conter 1 ator, quantidade encontrada: " + atores.size());
-        System.out.println("Primeiro ator deve ser 'Will Smith', valor encontrado: " + atores.get(0).getNome());
-        System.out.println("Primeiro ator deve ser id '1', valor encontrado: " + atores.get(0).getId());
-
-        /*####### Testes Consultar Ator #######*/
-//        TestAtor.cadastraDados();
-//        TestAtor.testConsultarAtorId();
-//        TestAtor.buscarTodosAtores();
-//        TestAtor.listarAtoresComFiltro();
-        /*####### Testes Consultar Diretor #######*/
-//        TestDiretor.cadastraDados();
-//        TestDiretor.testConsultarDiretorId();
-//        TestDiretor.buscarTodosDiretores();
-
+        SpringApplication.run(Aplicacao.class, args);
     }
 }
