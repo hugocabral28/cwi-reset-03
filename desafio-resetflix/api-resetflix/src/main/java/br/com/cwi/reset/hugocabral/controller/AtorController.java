5b5320dff2d8f76cb51de101d74ba238309d18dc
hugo.cabral <hugocabral2803@gmail.com>
2021-10-20 00:45:33 -0300

    Update classe AtorController

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/controller/AtorController.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/controller/AtorController.java
@@ -3,7 +3,6 @@ package br.com.cwi.reset.hugocabral.controller;
 import br.com.cwi.reset.hugocabral.FakeDatabase;
 import br.com.cwi.reset.hugocabral.domain.Ator;
 import br.com.cwi.reset.hugocabral.domain.AtorEmAtividade;
-import br.com.cwi.reset.hugocabral.exception.ExceptionConsultaId;
 import br.com.cwi.reset.hugocabral.request.AtorRequest;
 import br.com.cwi.reset.hugocabral.service.AtorService;
 import org.springframework.http.HttpStatus;
@@ -24,22 +23,22 @@ public class AtorController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void criarAtor(@RequestBody AtorRequest atorRequest) throws Exception{
+    public void criarAtor(@RequestBody AtorRequest atorRequest) throws Exception {
         this.atorService.criarAtor(atorRequest);
     }
 
     @GetMapping("/em_atividade")
-    public List<AtorEmAtividade> listarAtoresEmAtividade(@RequestParam Optional<String> filtroNome) throws Exception{
-        return atorService.listarAtoresEmAtividade(filtroNome);
+    public List<AtorEmAtividade> listarAtoresEmAtividade(@RequestParam Optional<String> filtroNome) throws Exception {
+        return this.atorService.listarAtoresEmAtividade(filtroNome);
     }
 
     @GetMapping("/{id}")
-    public Ator consultarAtor(@PathVariable Integer id) throws Exception, ExceptionConsultaId {
+    public Ator consultarAtor(@PathVariable Integer id) throws Exception {
         return this.atorService.consultarAtor(id);
     }
 
     @GetMapping
-    public List<Ator> consultarAtores(AtorRequest atorRequest) throws Exception{
-        return atorService.consultarAtores();
+    public List<Ator> consultarAtores(AtorRequest atorRequest) throws Exception {
+        return this.atorService.consultarAtores();
     }
 }
--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/service/AtorService.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/service/AtorService.java
@@ -10,9 +10,10 @@ import br.com.cwi.reset.hugocabral.exception.*;
 import br.com.cwi.reset.hugocabral.request.AtorRequest;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class AtorService {
 
@@ -23,72 +24,85 @@ public class AtorService {
         this.fakeDatabase = fakeDatabase;
     }
 
-    public void criarAtor(AtorRequest atorRequest) throws ExceptionCampoInvalido,
-            ExceptionNomeESobrenome,ExceptionDataDeNascimento,
-            ExceptionAnoInicioAtividade,ExceptionCadastroDuplicado{
+    public void criarAtor(AtorRequest atorRequest) throws CampoObrigatorioException,
+            NomeESobrenomeException, DataDeNascimentoInvalidaException,
+            AnoInicioAtividadeInvalidoException, CadastroDuplicadoException {
 
-            validaCamposObrigatorios(atorRequest);
-            validaNomeESobrenome(atorRequest);
-            validaDataNascimento(atorRequest);
-            validaAnoInicioAtividade(atorRequest);
-            validaDuplicidadeCadastro(atorRequest);
+        validaCamposObrigatorios(atorRequest);
+        validaNomeESobrenome(atorRequest);
+        validaDataNascimento(atorRequest);
+        validaAnoInicioAtividade(atorRequest);
+        validaDuplicidadeCadastro(atorRequest);
 
-            sequenceIdAtor = gerarIdAtor();
-            final Ator ator = new Ator(sequenceIdAtor, atorRequest.getNome(), atorRequest.getDataNascimento(), atorRequest.getStatusCarreira(), atorRequest.getAnoInicioAtividade());
-            fakeDatabase.persisteAtor(ator);
+        sequenceIdAtor = gerarIdAtor();
+
+        final Ator ator = new Ator(sequenceIdAtor, atorRequest.getNome(), atorRequest.getDataNascimento(), atorRequest.getStatusCarreira(), atorRequest.getAnoInicioAtividade());
+        fakeDatabase.persisteAtor(ator);
 
     }
 
     // Demais métodos da classe
-    public List<AtorEmAtividade> listarAtoresEmAtividade(Optional<String> filtroNome) throws ExceptionFiltroNome {
-        List<Ator> atores = fakeDatabase.recuperaAtores();
+    public List<AtorEmAtividade> listarAtoresEmAtividade(Optional<String> filtroNome) throws Exception {
+        final List<Ator> atoresCadastrados = fakeDatabase.recuperaAtores();
 
-        StatusCarreira statusEmAtividade = StatusCarreira.EM_ATIVIDADE;
-        boolean filtroComNome = filtroNome.isPresent();
-        String[] nome = {};
-
-        if (filtroComNome) {
-            //Caso tenha espaços em brancos remove do inicio e final
-            nome = filtroNome.toString().trim().split(" ");
-        }
-        //Caso tenha filtroNome filtrar ator pelo Nome
-        List<AtorEmAtividade> atoresFiltrados = atores.stream()
-                .filter(ator -> filtroComNome ? ator.getNome().contains(filtroNome.get()) : true)
-                .filter(ator -> ator.getStatusCarreira().equals(statusEmAtividade))
-                .map(ator -> new AtorEmAtividade(ator.getId(), ator.getNome(), ator.getDataNascimento()))
-                .collect(Collectors.toList());
-
-
-        if (atoresFiltrados.isEmpty()) {
-            String filtro = filtroNome.map(Object::toString).orElse(null);
-            throw new ExceptionFiltroNome("Ator", filtro);
+        if (atoresCadastrados.isEmpty()) {
+            throw new SemCadastroException(TipoDominioException.ATOR.getSingular(), TipoDominioException.ATOR.getPlural());
         }
 
-        return atoresFiltrados;
+        final List<AtorEmAtividade> retorno = new ArrayList<>();
 
-    }
+        if (filtroNome.isPresent()) {
+            for (Ator ator : atoresCadastrados) {
+                //Verificando se o filtroNome informado contem no nome
+                final boolean containsFilter = atoresCadastrados.stream()
+                        .allMatch(atorlistadoFilter -> ator.getNome()
+                                .toLowerCase(Locale.ROOT)
+                                .contains(filtroNome.get().toLowerCase(Locale.ROOT)));
 
-    public Ator consultarAtor(Integer id) throws Exception, ExceptionConsultaId {
-
-        if (id == null) {
-            throw new ExceptionIdObrigatorio("id");
-        }
-        final List<Ator> atores = fakeDatabase.recuperaAtores();
-
-            for (Ator ator : atores) {
-                if (id.equals(ator.getId())) {
-                    return ator;
+                final boolean emAtividade = StatusCarreira.EM_ATIVIDADE.equals(ator.getStatusCarreira());
+                if (containsFilter && emAtividade) {
+                    retorno.add(new AtorEmAtividade(ator.getId(), ator.getNome(), ator.getDataNascimento()));
                 }
             }
+        } else {
+            for (Ator ator : atoresCadastrados) {
+                final boolean emAtividade = StatusCarreira.EM_ATIVIDADE.equals(ator.getStatusCarreira());
+                if (emAtividade) {
+                    retorno.add(new AtorEmAtividade(ator.getId(), ator.getNome(), ator.getDataNascimento()));
+                }
+            }
+        }
 
-            throw new ExceptionConsultaId(TipoDominioException.ATOR.getSingular(), id);
+        if (retorno.isEmpty() && filtroNome != null) {
+            throw new FiltroNomeException(TipoDominioException.ATOR.getSingular(), filtroNome.get());
+        }
+
+        return retorno;
 
     }
 
-    public List<Ator> consultarAtores() throws ExceptionSemCadastro {
+    public Ator consultarAtor(Integer id) throws Exception {
+
+        if (id == null) {
+            throw new CampoObrigatorioException("id");
+        }
+
+        final List<Ator> atores = fakeDatabase.recuperaAtores();
+
+        for (Ator ator : atores) {
+            if (id.equals(ator.getId())) {
+                return ator;
+            }
+        }
+
+        throw new ConsultaIdException(TipoDominioException.ATOR.getSingular(), id);
+
+    }
+
+    public List<Ator> consultarAtores() throws SemCadastroException {
         List<Ator> list = fakeDatabase.recuperaAtores();
         if (list.isEmpty()) {
-            throw new ExceptionSemCadastro(TipoDominioException.ATOR.getSingular(),TipoDominioException.ATOR.getPlural());
+            throw new SemCadastroException(TipoDominioException.ATOR.getSingular(), TipoDominioException.ATOR.getPlural());
         }
         return list;
     }
@@ -97,61 +111,61 @@ public class AtorService {
         return ++sequenceIdAtor;
     }
 
-    private void validaCamposObrigatorios(AtorRequest atorRequest) throws ExceptionCampoInvalido {
+    private void validaCamposObrigatorios(AtorRequest atorRequest) throws CampoObrigatorioException {
 
         String nome = atorRequest.getNome();
         if (nome == null || nome.equals("")) {
-            throw new ExceptionCampoInvalido(Constantes.CAMPO_NOME);
+            throw new CampoObrigatorioException(Constantes.CAMPO_NOME);
         }
 
         LocalDate dataNascimento = atorRequest.getDataNascimento();
         if (dataNascimento == null) {
-            throw new ExceptionCampoInvalido(Constantes.CAMPO_DATA_NASCIMENTO);
+            throw new CampoObrigatorioException(Constantes.CAMPO_DATA_NASCIMENTO);
         }
 
         Integer anoInicioAtividade = atorRequest.getAnoInicioAtividade();
         if (anoInicioAtividade == null) {
-            throw new ExceptionCampoInvalido(Constantes.CAMPO_ANO_INICIO_ATIVIDADE);
+            throw new CampoObrigatorioException(Constantes.CAMPO_ANO_INICIO_ATIVIDADE);
         }
 
         StatusCarreira statusCarreira = atorRequest.getStatusCarreira();
         if (statusCarreira == null) {
-            throw new ExceptionCampoInvalido(Constantes.CAMPO_STATUS_CARREIRA);
+            throw new CampoObrigatorioException(Constantes.CAMPO_STATUS_CARREIRA);
         }
 
     }
 
-    private void validaNomeESobrenome(AtorRequest atorRequest) throws ExceptionNomeESobrenome {
+    private void validaNomeESobrenome(AtorRequest atorRequest) throws NomeESobrenomeException {
         String[] nomeSobrenome = atorRequest.getNome().split(" ");
         if (nomeSobrenome.length < 2) {
-            throw new ExceptionNomeESobrenome(TipoDominioException.ATOR.getSingular());
+            throw new NomeESobrenomeException(TipoDominioException.ATOR.getSingular());
         }
     }
 
-    private void validaDataNascimento(AtorRequest atorRequest) throws ExceptionDataDeNascimento {
+    private void validaDataNascimento(AtorRequest atorRequest) throws DataDeNascimentoInvalidaException {
         Integer anoAtual = LocalDate.now().getYear();
         Integer anoNascimento = atorRequest.getDataNascimento().getYear();
 
         if (anoNascimento > anoAtual) {
-            throw new ExceptionDataDeNascimento(TipoDominioException.ATOR.getSingular());
+            throw new DataDeNascimentoInvalidaException(TipoDominioException.ATOR.getSingular());
         }
     }
 
-    private void validaAnoInicioAtividade(AtorRequest atorRequest) throws ExceptionAnoInicioAtividade {
+    private void validaAnoInicioAtividade(AtorRequest atorRequest) throws AnoInicioAtividadeInvalidoException {
         Integer anoNascimento = atorRequest.getDataNascimento().getYear();
         Integer anoInicioAtividade = atorRequest.getAnoInicioAtividade();
 
         if (anoInicioAtividade < anoNascimento) {
-            throw new ExceptionAnoInicioAtividade(TipoDominioException.ATOR.getSingular());
+            throw new AnoInicioAtividadeInvalidoException(TipoDominioException.ATOR.getSingular());
         }
     }
 
-    private void validaDuplicidadeCadastro(AtorRequest atorRequest) throws ExceptionCadastroDuplicado {
+    private void validaDuplicidadeCadastro(AtorRequest atorRequest) throws CadastroDuplicadoException {
         String nomeDoAtor = atorRequest.getNome();
         List<Ator> atores = fakeDatabase.recuperaAtores();
         for (Ator ator : atores) {
-            if (ator.getNome().equals(nomeDoAtor)) {
-                throw new ExceptionCadastroDuplicado(TipoDominioException.ATOR.getSingular(), nomeDoAtor);
+            if (ator.getNome().toLowerCase(Locale.ROOT).equals(nomeDoAtor.toLowerCase(Locale.ROOT))) {
+                throw new CadastroDuplicadoException(TipoDominioException.ATOR.getSingular(), nomeDoAtor);
             }
         }
     }
