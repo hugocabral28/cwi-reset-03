e0ae47126a37989a36aaf8824c6ffe3e16b57dd5
hugo.cabral <hugocabral2803@gmail.com>
2021-10-20 00:48:01 -0300

    Create class DiretorController

---
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/controller/DiretorController.java
@@ -0,0 +1,38 @@
+package br.com.cwi.reset.hugocabral.controller;
+
+import br.com.cwi.reset.hugocabral.FakeDatabase;
+import br.com.cwi.reset.hugocabral.domain.Diretor;
+import br.com.cwi.reset.hugocabral.request.DiretorRequest;
+import br.com.cwi.reset.hugocabral.service.DiretorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/diretores")
+public class DiretorController {
+    private DiretorService diretorService;
+
+    public DiretorController() {
+        this.diretorService = new DiretorService(FakeDatabase.getInstance());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarDiretor(@RequestBody DiretorRequest diretorRequest) throws Exception {
+        this.diretorService.cadastrarDiretor(diretorRequest);
+    }
+
+    @GetMapping
+    public List<Diretor> listarDiretores(@RequestParam Optional<String> filtroNome) throws Exception {
+        return this.diretorService.listarDiretores(filtroNome);
+    }
+
+    @GetMapping("/{id}")
+    public Diretor consultarDiretor(@PathVariable Integer id) throws Exception {
+        return this.diretorService.consultarDiretor(id);
+    }
+
+}
--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/service/DiretorService.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/service/DiretorService.java
@@ -7,7 +7,9 @@ import br.com.cwi.reset.hugocabral.exception.*;
 import br.com.cwi.reset.hugocabral.request.DiretorRequest;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 
@@ -20,121 +22,135 @@ public class DiretorService {
         this.fakeDatabase = fakeDatabase;
     }
 
-    public void cadastrarDiretor(DiretorRequest diretorRequest) {
-        try {
-            validaCamposObrigatorios(diretorRequest);
-            validaNomeESobrenome(diretorRequest);
-            validaDataNascimento(diretorRequest);
-            validaAnoInicioAtividade(diretorRequest);
-            validaDuplicidadeCadastro(diretorRequest);
-            sequenceIdDiretor = gerarIdAtor();
-            final Diretor diretor = new Diretor(sequenceIdDiretor, diretorRequest.getNome(), diretorRequest.getDataNascimento(), diretorRequest.getAnoInicioAtividade());
-            fakeDatabase.persisteDiretor(diretor);
+    public void cadastrarDiretor(DiretorRequest diretorRequest) throws CampoObrigatorioException,
+            NomeESobrenomeException, DataDeNascimentoInvalidaException,
+            AnoInicioAtividadeInvalidoException, CadastroDuplicadoException {
 
-        }//Caso ExceptionCampoInvalido
-        catch (ExceptionCampoInvalido e) {
-            e.printStackTrace();
-        }
-        //Caso ExceptionNomeESobrenome
-        catch (ExceptionNomeESobrenome e) {
-            e.printStackTrace();
-        }
-        //Caso ExceptionDataDeNascimento
-        catch (ExceptionDataDeNascimento e) {
-            e.printStackTrace();
-        }
-        //Caso ExceptionAnoInicioAtividade
-        catch (ExceptionAnoInicioAtividade e) {
-            e.printStackTrace();
-        }
-        //Caso ExceptionCadastroDuplicado
-        catch (ExceptionCadastroDuplicado e) {
-            e.printStackTrace();
-        }
+        validaCamposObrigatorios(diretorRequest);
+        validaNomeESobrenome(diretorRequest);
+        validaDataNascimento(diretorRequest);
+        validaAnoInicioAtividade(diretorRequest);
+        validaDuplicidadeCadastro(diretorRequest);
+        sequenceIdDiretor = gerarIdDiretor();
+
+        final Diretor diretor = new Diretor(sequenceIdDiretor, diretorRequest.getNome(), diretorRequest.getDataNascimento(), diretorRequest.getAnoInicioAtividade());
+        fakeDatabase.persisteDiretor(diretor);
     }
 
     // Demais métodos da classe
-    public Optional<Diretor> consultarDiretor(Integer id) throws ExceptionIdObrigatorio {
+    public List<Diretor> listarDiretores(Optional<String> filtroNome) throws Exception {
+        final List<Diretor> diretoresCadastrados = fakeDatabase.recuperaDiretores();
 
-        if (id == null) {
-            throw new ExceptionIdObrigatorio("id");
+        if (diretoresCadastrados.isEmpty()) {
+            throw new SemCadastroException(TipoDominioException.DIRETOR.getSingular(), TipoDominioException.DIRETOR.getPlural());
         }
 
-        List<Diretor> diretores = fakeDatabase.recuperaDiretores();
-        //filtrando Diretor pelo id
-        Optional<Diretor> diretorEncontrado = diretores.stream()
-                .filter(diretor -> id.equals(diretor.getId()))
-                .findFirst();
+        final List<Diretor> retorno = new ArrayList<>();
 
-        if (!diretorEncontrado.isPresent()) {
-            throw new ExceptionIdObrigatorio("id");
+        if (filtroNome.isPresent()) {
+            for (Diretor diretor : diretoresCadastrados) {
+                //Verificando se o filtroNome informado contem no nome
+                final boolean containsFilter = diretoresCadastrados.stream()
+                        .allMatch(atorlistadoFilter -> diretor.getNome()
+                                .toLowerCase(Locale.ROOT)
+                                .contains(filtroNome.get().toLowerCase(Locale.ROOT)));
+
+                if (containsFilter) {
+                    retorno.add(diretor);
+                }
+            }
+        } else {
+            retorno.addAll(diretoresCadastrados);
         }
 
-        return diretorEncontrado;
+        if (retorno.isEmpty() && filtroNome != null) {
+            throw new FiltroNomeException(TipoDominioException.DIRETOR.getSingular(), filtroNome.get());
+        }
+
+        return retorno;
     }
 
-    public List<Diretor> consultarDiretor() throws ExceptionSemCadastro {
+
+    public Diretor consultarDiretor(Integer id) throws Exception {
+
+        if (id == null) {
+            throw new CampoObrigatorioException("id");
+        }
+
+        final List<Diretor> diretores = fakeDatabase.recuperaDiretores();
+
+        for (Diretor diretore : diretores) {
+            if (id.equals(diretore.getId())) {
+                return diretore;
+            }
+        }
+
+        throw new ConsultaIdException(TipoDominioException.DIRETOR.getSingular(), id);
+
+    }
+
+    public List<Diretor> consultarDiretor() throws SemCadastroException {
         List<Diretor> list = fakeDatabase.recuperaDiretores();
         if (list.isEmpty()) {
-            throw new ExceptionSemCadastro(TipoDominioException.DIRETOR.getSingular(),TipoDominioException.DIRETOR.getPlural());
+            throw new SemCadastroException(TipoDominioException.DIRETOR.getSingular(), TipoDominioException.DIRETOR.getPlural());
         }
         return list;
     }
 
-    private Integer gerarIdAtor() {
+    private Integer gerarIdDiretor() {
         return ++sequenceIdDiretor;
     }
 
-    private void validaCamposObrigatorios(DiretorRequest diretorRequest) throws ExceptionCampoInvalido {
+    private void validaCamposObrigatorios(DiretorRequest diretorRequest) throws CampoObrigatorioException {
 
         String nome = diretorRequest.getNome();
         if (nome == null || nome.equals("")) {
-            throw new ExceptionCampoInvalido(Constantes.CAMPO_NOME);
+            throw new CampoObrigatorioException(Constantes.CAMPO_NOME);
         }
 
         LocalDate dataNascimento = diretorRequest.getDataNascimento();
         if (dataNascimento == null) {
-            throw new ExceptionCampoInvalido(Constantes.CAMPO_DATA_NASCIMENTO);
+            throw new CampoObrigatorioException(Constantes.CAMPO_DATA_NASCIMENTO);
         }
 
         Integer anoInicioAtividade = diretorRequest.getAnoInicioAtividade();
         if (anoInicioAtividade == null) {
-            throw new ExceptionCampoInvalido(Constantes.CAMPO_ANO_INICIO_ATIVIDADE);
+            throw new CampoObrigatorioException(Constantes.CAMPO_ANO_INICIO_ATIVIDADE);
         }
 
     }
 
-    private void validaNomeESobrenome(DiretorRequest diretorRequest) throws ExceptionNomeESobrenome {
+    private void validaNomeESobrenome(DiretorRequest diretorRequest) throws NomeESobrenomeException {
         String[] nomeSobrenome = diretorRequest.getNome().trim().split(" ");
         if (nomeSobrenome.length < 2) {
-            throw new ExceptionNomeESobrenome(TipoDominioException.DIRETOR.getSingular());
+            throw new NomeESobrenomeException(TipoDominioException.DIRETOR.getSingular());
         }
     }
 
-    private void validaDataNascimento(DiretorRequest diretorRequest) throws ExceptionDataDeNascimento {
+    private void validaDataNascimento(DiretorRequest diretorRequest) throws DataDeNascimentoInvalidaException {
         Integer anoAtual = LocalDate.now().getYear();
         Integer anoNascimento = diretorRequest.getDataNascimento().getYear();
 
         if (anoNascimento > anoAtual) {
-            throw new ExceptionDataDeNascimento(TipoDominioException.DIRETOR.getSingular());
+            throw new DataDeNascimentoInvalidaException(TipoDominioException.DIRETOR.getSingular());
         }
     }
 
-    private void validaAnoInicioAtividade(DiretorRequest diretorRequest) throws ExceptionAnoInicioAtividade {
+    private void validaAnoInicioAtividade(DiretorRequest diretorRequest) throws AnoInicioAtividadeInvalidoException {
         Integer anoNascimento = diretorRequest.getDataNascimento().getYear();
         Integer anoInicioAtividade = diretorRequest.getAnoInicioAtividade();
 
         if (anoInicioAtividade < anoNascimento) {
-            throw new ExceptionAnoInicioAtividade(TipoDominioException.DIRETOR.getSingular());
+            throw new AnoInicioAtividadeInvalidoException(TipoDominioException.DIRETOR.getSingular());
         }
     }
 
-    private void validaDuplicidadeCadastro(DiretorRequest diretorRequest) throws ExceptionCadastroDuplicado {
+    private void validaDuplicidadeCadastro(DiretorRequest diretorRequest) throws CadastroDuplicadoException {
         String nomeDoDiretor = diretorRequest.getNome();
         List<Diretor> diretores = fakeDatabase.recuperaDiretores();
         for (Diretor diretor : diretores) {
-            if (diretor.getNome().equals(nomeDoDiretor)) {
-                throw new ExceptionCadastroDuplicado(TipoDominioException.DIRETOR.getSingular(), nomeDoDiretor);
+            if (diretor.getNome().toLowerCase(Locale.ROOT).equals(nomeDoDiretor.toLowerCase(Locale.ROOT))) {
+                throw new CadastroDuplicadoException(TipoDominioException.DIRETOR.getSingular(), nomeDoDiretor);
             }
         }
     }
