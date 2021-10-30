36f08e0415735cd6857f32e7afd6b452f35c7a9b
hugo.cabral <hugocabral2803@gmail.com>
2021-10-30 00:09:36 -0300

    Refactor exceptions

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/exception/FiltroFilmesException.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/hugocabral/exception/FiltroFilmesException.java
@@ -2,10 +2,10 @@ package br.com.cwi.reset.hugocabral.exception;
 
 public class FiltroFilmesException extends Exception {
     public FiltroFilmesException(String tipo, String nomeFilme, String nomeDiretor, String nomePersonagem, String nomeAtor) {
-        super(String.format("%s não encontrado com os filtros nomeFilme= %s," +
-                " nomeDiretor= %s," +
-                " nomePersonagem= %s," +
-                " nomeAtor= %s," +
+        super(String.format("%s não encontrado com os filtros nomeFilme = %s," +
+                " nomeDiretor = %s," +
+                " nomePersonagem = %s," +
+                " nomeAtor = %s," +
                 " favor informar outros filtros.", tipo, nomeFilme, nomeDiretor, nomePersonagem, nomeAtor));
     }
 }
